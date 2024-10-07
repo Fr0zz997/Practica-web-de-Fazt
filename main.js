9c96925dfd63dcbc1188aa9a3ecef58223326f91
Fr0zz997 <86337479+Fr0zz997@users.noreply.github.com>
2024-10-07 19:04:13 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.querySelector(".menu-btn").addEventListener("click",  () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+})
+
+ScrollReveal().reveal('.newscard');
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.social-icons');
+ScrollReveal().reveal('.banerContainer');
+ScrollReveal().reveal('.footer');
